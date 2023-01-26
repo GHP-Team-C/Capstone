23bb1090fcab36e90de548eaef553640d486fab4
Zoe Moersch <zoechm@proton.me>
2023-01-26 11:08:47 -0600

    fix: add babel-plugin-import to minimize mui's impact on webpack size

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,39 @@
 module.exports = {
-  entry: ['./client/index.js'],
+  entry: ["./client/index.js"],
   output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js',
+    path: __dirname + "/public",
+    filename: "bundle.js",
   },
   context: __dirname,
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: [
+            [
+              "import",
+              {
+                libraryName: "@mui/material",
+                libraryDirectory: "",
+                camel2DashComponentName: false,
+              },
+              "core",
+            ],
+            [
+              "import",
+              {
+                libraryName: "@mui/icons-material",
+                libraryDirectory: "",
+                camel2DashComponentName: false,
+              },
+              "icons",
+            ],
+          ],
         },
       },
     ],
