cc9f3876b5ef6c2b2e92c52747a7eedaaa468251
Zoe Moersch <zoechm@proton.me>
2023-01-30 10:28:40 -0600

    refactor: rerender music staff without duplication

--- client/features/lessons/MusicalStaff.js
+++ client/features/lessons/MusicalStaff.js
@@ -1,73 +1,71 @@
 import React, { useEffect } from "react";
 import * as Vex from "vexflow";
 
-
-
-
-const MusicalStaff = ({note}) => {
-
-
-  // useEffect(()=>{
-  //   const staffDiv = document.getElementById('staff')
-  // document.body.removeChild(staffDiv)}, [note])
-
-  const { Renderer, Stave, Formatter, StaveNote, Voice } = Vex.Flow
+const MusicalStaff = ({ note }) => {
+  const { Renderer, Stave, Formatter, StaveNote, Voice } = Vex.Flow;
 
   // Create an SVG renderer and attach it to the DIV element named "boo".
-  const staff = document.createElement('div');
-  staff.setAttribute('id', 'staff')
-  document.body.appendChild(staff)
+  // const staff = document.createElement("div");
+  // staff.setAttribute("id", "staffDiv");
+  // document.body.appendChild(staff);
 
+  let div = document.getElementById("staffDiv");
 
-  const div = document.getElementById("staff");
-  const renderer = new Renderer(div, Renderer.Backends.SVG);
+  useEffect(() => {
+    div = document.getElementById("staffDiv");
+  }, [note]);
 
-  // Configure the rendering context.
-  renderer.resize(500, 200);
-  const context = renderer.getContext();
+  useEffect(() => {
+    if (div) {
+      const renderer = new Renderer(div, Renderer.Backends.SVG);
+      renderer.ctx.element.children[0].setAttribute("id", "staff");
 
-  // Create a stave of width 400 at position 10, 40 on the canvas.
-const stave = new Stave(10, 40, 400);
+      // Configure the rendering context.
+      renderer.resize(500, 200);
+      const context = renderer.getContext();
 
-// Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("4/4");
+      // Create a stave of width 400 at position 10, 40 on the canvas.
+      const stave = new Stave(10, 40, 400);
 
-// Connect it to the rendering context and draw!
-stave.setContext(context).draw();
+      // Add a clef and time signature.
+      stave.addClef("treble").addTimeSignature("4/4");
 
+      // Connect it to the rendering context and draw!
+      stave.setContext(context).draw();
 
-const notes = [
-  // A quarter-note C.
-  new StaveNote({ keys: [`${note}/4`], duration: "q" }),
+      const notes = [
+        // A quarter-note C.
+        new StaveNote({ keys: [`${note}/4`], duration: "q" }),
 
-  // A quarter-note D.
-  new StaveNote({ keys: ["d/4"], duration: "q" }),
+        // A quarter-note D.
+        new StaveNote({ keys: ["d/4"], duration: "q" }),
 
-  // A quarter-note rest. Note that the key (b/4) specifies the vertical
-  // position of the rest.
-  new StaveNote({ keys: ["b/4"], duration: "qr" }),
+        // A quarter-note rest. Note that the key (b/4) specifies the vertical
+        // position of the rest.
+        new StaveNote({ keys: ["b/4"], duration: "qr" }),
 
-  // A C-Major chord.
-  new StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" }),
-];
+        // A C-Major chord.
+        new StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" }),
+      ];
 
-// Create a voice in 4/4 and add above notes
-const voice = new Voice({ num_beats: 4, beat_value: 4 });
-voice.addTickables(notes);
+      // Create a voice in 4/4 and add above notes
+      const voice = new Voice({ num_beats: 4, beat_value: 4 });
+      voice.addTickables(notes);
 
-// Format and justify the notes to 400 pixels.
-new Formatter().joinVoices([voice]).format([voice], 350);
+      // Format and justify the notes to 400 pixels.
+      new Formatter().joinVoices([voice]).format([voice], 350);
 
-// Render voice
-voice.draw(context, stave);
+      // Render voice
+      voice.draw(context, stave);
+    }
+  }, [div, note]);
 
   return (
     <div>
-    <h1>Musical Staff</h1>
-    <div id="staff"></div>
+      <h1>Musical Staff</h1>
+      <div id="staffDiv"></div>
     </div>
-  )
-}
+  );
+};
 
-export default MusicalStaff
-
+export default MusicalStaff;
--- client/features/lessons/StaffForm.js
+++ client/features/lessons/StaffForm.js
@@ -1,33 +1,38 @@
 import React, { useState } from "react";
-import { InputLabel, MenuItem, FormControl, Select}from '@mui/material';
+import { InputLabel, MenuItem, FormControl, Select } from "@mui/material";
 import MusicalStaff from "./MusicalStaff";
 
-
-export default function StaffForm(){
-  const [note, setNote] = useState('c');
-  
+export default function StaffForm() {
+  const [note, setNote] = useState("c");
 
   const handleChange = (event) => {
     setNote(event.target.value);
+    const staffSvg = document.getElementById("staff");
+    const staffDiv = document.getElementById("staffDiv");
+    if (staffSvg) staffDiv.removeChild(staffSvg);
   };
 
-  const noteArray = ["c", "d", "e", "f", "g", "a", "b"]
+  const noteArray = ["c", "d", "e", "f", "g", "a", "b"];
 
-  return(
+  return (
     <div>
       <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="demo-select-small">Note</InputLabel>
-      <Select
-        labelId="demo-select-small"
-        id="demo-select-small"
-        value={note}
-        label="note"
-        onChange={handleChange}
-      >
-     {noteArray.map((note)=> <MenuItem key={note} value={note}>{note}</MenuItem>)}
-      </Select>
-    </FormControl>
-    <MusicalStaff note={note}/>
-       </div>
-  )
+        <InputLabel id="demo-select-small">Note</InputLabel>
+        <Select
+          labelId="demo-select-small"
+          id="demo-select-small"
+          value={note}
+          label="note"
+          onChange={handleChange}
+        >
+          {noteArray.map((note) => (
+            <MenuItem key={note} value={note}>
+              {note}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <MusicalStaff note={note} />
+    </div>
+  );
 }
