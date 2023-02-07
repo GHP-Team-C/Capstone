376d46f6fc07d29452e5738c1e40c00f4e2e7bc9
maemay85 <hellomaemay@gmail.com>
2023-02-07 15:51:49 -0500

    unsatisfyingly fixed the seed issue ¯\_(ツ)_/¯

--- script/data/staff.js
+++ script/data/staff.js
@@ -1,34 +1,10 @@
-module.exports=[
-  {
-    timeSig: "4/4",
-    clef: "treble"
-  },
-  {
-    timeSig: "4/4",
-    clef: "treble"
-  },
-  {
-    timeSig: "4/4",
-    clef: "treble"
-  },
-  {
-    timeSig: "4/4",
-    clef: "treble"
-  },
-  {
-    timeSig: "4/4",
-    clef: "treble"
-  },
-  {
-    timeSig: "4/4",
-    clef: "treble"
-  },
-  {
-    timeSig: "4/4",
-    clef: "treble"
-  },
-  {
-    timeSig: "4/4",
-    clef: "treble"
-  },
-]
+const trebleClef = {
+  timeSig: "4/4",
+  clef: "treble"
+}
+
+let staffs = []
+
+for (let i=0; i<28; ++i) staffs.push(trebleClef)
+
+module.exports=staffs
