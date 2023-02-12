829fc6a9686cdbf19dd8e758ea67c6dde95f30e6
Shoshana Levitt <shoshana.levitt@gmail.com>
2023-02-12 12:03:20 -0500

    added icons to User Profile

--- client/features/users/UserProfile.js
+++ client/features/users/UserProfile.js
@@ -2,8 +2,15 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSingleUser } from "./singleUserSlice";
 import { Link } from "react-router-dom";
-import { Typography, Box, ButtonGroup, Button } from "@mui/material";
-import { Person, DesignServices } from "@mui/icons-material";
+import {
+  Typography,
+  Box,
+  ButtonGroup,
+  Button,
+  Stack,
+  Grid,
+} from "@mui/material";
+import { Person, DesignServices, Badge, Mail } from "@mui/icons-material";
 import { black, grey } from "@mui/material/colors";
 
 const UserProfile = () => {
@@ -53,9 +60,27 @@ const UserProfile = () => {
         <Typography variant="h3">
           Welcome, {firstName} {lastName}!
         </Typography>
+        <Box mt={2} mb={2}>
+          <Typography
+            variant="h6"
+            component={Stack}
+            direction="row"
+            alignItems="center"
+          >
+            <Badge fontSize="inherit" sx={{ marginRight: 1 }} />
+            Username: {username}
+          </Typography>
+          <Typography
+            variant="h6"
+            component={Stack}
+            direction="row"
+            alignItems="center"
+          >
+            <Mail fontSize="inherit" sx={{ marginRight: 1 }} />
+            Email: {email}
+          </Typography>
+        </Box>
         <img src={avatarUrl} height="200px" />
-        <Typography variant="h6">Username: {username}</Typography>
-        <Typography variant="h6">Email: {email}</Typography>
       </Box>
     </div>
   );
