cebc16e4f509ac7469fba21e8e03ac6a5402046a
Shoshana Levitt <shoshana.levitt@gmail.com>
2023-02-12 12:42:41 -0500

    added mui theme

--- client/index.js
+++ client/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './app/store.js';
-import App from './app/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
+import App from "./app/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById("app"));
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2a4783",
+    },
+    secondary: {
+      main: "#435679",
+    },
+  },
+});
 
 root.render(
   <Router>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </Router>
 );
